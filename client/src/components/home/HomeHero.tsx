95bd4b62f3fd508de04d5d05df5381e81f8622a4
Himesh <35775892+himesh220002@users.noreply.github.com>
2026-03-08 19:42:00 +0530

    Update HomeHero.tsx

--- client/src/components/home/HomeHero.tsx
+++ client/src/components/home/HomeHero.tsx
@@ -21,7 +21,7 @@ export default function HomeHero() {
     const clinicYears = Math.max(0, currentYear - estYear);
 
     return (
-        <section className="relative min-h-[550px] sm:min-h-[600px] h-[calc(100vh-64px)] overflow-hidden shadow-[0_0px_20px_-16px_rgba(0,0,0,0.2)] 2xl:rounded-3xl flex items-start md:items-center group ">
+        <section className="relative min-h-[550px] sm:min-h-[600px] h-[calc(100vh-64px)] overflow-hidden shadow-[0_0px_20px_-16px_rgba(0,0,0,0.2)] 2xl:rounded-3xl flex items-center md:items-center group ">
             {/* Immersive Background */}
             <div className="absolute inset-0">
                 <img
