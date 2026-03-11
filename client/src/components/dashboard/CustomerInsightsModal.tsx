684230745500d9d413afb6be3fa48033bd30080d
Himesh Satyam <satyamhimesh@gmail.com>
2026-03-11 22:33:51 +0530

    a

--- client/src/components/dashboard/CustomerInsightsModal.tsx
+++ client/src/components/dashboard/CustomerInsightsModal.tsx
@@ -216,7 +216,7 @@ export default function CustomerInsightsModal({ isOpen, onClose, patients, appoi
                 </Transition.Child>
 
                 <div className="fixed inset-0 overflow-y-auto">
-                    <div className="flex min-h-full items-center justify-center p-2 sm:p-4">
+                    <div className="flex min-h-full items-center justify-center p-2 sm:p-4 mx-2 sm:mx-0">
                         <Transition.Child
                             as={Fragment}
                             enter="ease-out duration-300"
