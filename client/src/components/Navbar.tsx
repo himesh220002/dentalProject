65725cbb0f0c452af04e8c4101d2efbbcd01e4c5
Himesh Satyam <satyamhimesh@gmail.com>
2026-04-29 12:05:47 +0530

    a

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -130,7 +130,7 @@ export default function Navbar() {
     return (
         <>
             <nav className="bg-white/80 backdrop-blur-md sticky top-0 z-50 lg:z-51 shadow-sm border-b border-gray-100">
-                <div className="mx-20 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto xl:mx-20 px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16 items-center">
                         {/* Logo */}
                         <Link href="/" className="flex items-center space-x-2 group">
